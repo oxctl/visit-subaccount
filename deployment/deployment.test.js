8a1ff9e55a758cbb4c77f4ae8dfdb91338b56654
Adam Marshall <adam.marshall@it.ox.ac.uk>
2025-11-19 17:10:50 +0000

    Refine comments & text in deployment.test.js
    Updated comments for clarity in deployment tests.

--- deployment/deployment.test.js
+++ deployment/deployment.test.js
@@ -1,8 +1,6 @@
 import { test, expect } from '@playwright/test'
 import { dismissBetaBanner, getLtiIFrame, waitForNoSpinners, TEST_URL } from '@oxctl/deployment-test-utils'
 
-// TEST_URL is provided by @oxctl/deployment-test-utils and composes host+path
-
 test.describe('Test deployment', () => {
     test('The tool should load and the title "Redirecting to Subaccount..." should be shown (before redirecting to the subaccount).', async ({context, page}) => {
     await page.goto(TEST_URL)
@@ -10,7 +8,7 @@ test.describe('Test deployment', () => {
     const ltiIFrame = getLtiIFrame(page)
     await waitForNoSpinners(ltiIFrame)
 
-    // Check there's a title Account Reports
+    // Check there's the correct title on the page
     const title = ltiIFrame.getByText("Redirecting to Subaccount...")
     await expect(title).toBeVisible()
   })
